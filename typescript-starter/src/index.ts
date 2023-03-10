68d17f0ac93023f75fd7c66ac5dad7b642bb371e
yvandouenel <y.douenel@coopernet.fr>
2023-03-10 10:49:03 +0100

    Exo typescript-starter moved.

--- typescript-starter/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { getUser } from "./services/user";
-
-console.log('Hello world');
-// IFEES ou fonction anonyme immédiate
-(async function(){
-  const user = await getUser();
-  console.log(`user : `, user);
-})()
--- typescript-starter/src/services/user.ts
+++
@@ -1,5 +0,0 @@
-import user from "./../mocks/user.json";
-import { UserInterface } from './../models/User';
-export function getUser():Promise<UserInterface> {
-  return new Promise((resolve => resolve(user)))
-}
